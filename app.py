3e36a1036767c30806d62cbe31c70af10050168e
Kelsey Brahe Grover <kgrover3@myune.edu.au>
2024-04-22 15:24:23 -0600

    created second form with bounding box option that app.py can capture

--- app.py
+++ app.py
@@ -16,36 +16,42 @@ def hello_world():
         "index.html"
     )
 
+@app.route('/', methods=["GET", "POST"])
+def getVals():
+    if request.method == "POST":
+        boundingBox = request.form.get("boundingBox")
+    return render_template("index.html")
+
 # this allows the upload of a single file
 # nothing is done with it currently and it is not saved anywhere
-@app.route('/', methods=['GET', 'POST'])
-def predict_img():
-    if request.method == "POST":
-        if 'file' in request.files:
-            f = request.files['file']
-            basepath = os.path.dirname(__file__)
-            # save uploaded image into the uploads folder
-            filepath = os.path.join(basepath, 'uploads', f.filename)
-            f.save(filepath)
+# @app.route('/', methods=['GET', 'POST'])
+# def predict_img():
+#     if request.method == "POST":
+#         if 'file' in request.files:
+#             f = request.files['file']
+#             basepath = os.path.dirname(__file__)
+#             # save uploaded image into the uploads folder
+#             filepath = os.path.join(basepath, 'uploads', f.filename)
+#             f.save(filepath)
 
-            # perform image prediction
-            global imgpath
-            predict_img.imgpath = f.filename
-            img = cv2.imread(filepath)
-            frame = cv2.imencode('.jpg', cv2.UMat(img))[1].toBytes()
-            image = Image.open(io.BytesIO(frame))
-            yolo = YOLO('yolo_filename.pt')
-            detections = yolo.predict(image, save=True)
-            return display(f.filename)
+#             # perform image prediction
+#             global imgpath
+#             predict_img.imgpath = f.filename
+#             img = cv2.imread(filepath)
+#             frame = cv2.imencode('.jpg', cv2.UMat(img))[1].toBytes()
+#             image = Image.open(io.BytesIO(frame))
+#             yolo = YOLO('yolo_filename.pt')
+#             detections = yolo.predict(image, save=True)
+#             return display(f.filename)
 
-@app.route('/<path:filename>')
-def display(filename):
-    folder_path = 'runs/detect'
-    files = os.listdir(folder_path)
-    latest_file = files[0]
-    environ = request.environ
+# @app.route('/<path:filename>')
+# def display(filename):
+#     folder_path = 'runs/detect'
+#     files = os.listdir(folder_path)
+#     latest_file = files[0]
+#     environ = request.environ
 
-    return send_from_directory(folder_path, latest_file, environ)
+#     return send_from_directory(folder_path, latest_file, environ)
 
 # """Interacting with the classifier module"""
 # # First check whether the target directory already exists - this will save previous results being overwritten accidently. Consider having a popup confirm intention with user

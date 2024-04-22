e4a8ab976ba855f91c602969d84797f4f2c86876
Kelsey Brahe Grover <kgrover3@myune.edu.au>
2024-04-21 21:15:52 -0600

    Created some folders, added some functions to build off of

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask import render_template
+from PIL import Image
+from werkzeug.utils import send_from_directory
+import cv2
+import numpy as np
+import os
+from ultralytics import YOLO
 from classifer import get_img_classification, check_whether_target_directory_exists
 
 app = Flask(__name__)
@@ -10,6 +16,36 @@ def hello_world():
         "index.html"
     )
 
+# this allows the upload of a single file
+# nothing is done with it currently and it is not saved anywhere
+@app.route('/', methods=['GET', 'POST'])
+def predict_img():
+    if request.method == "POST":
+        if 'file' in request.files:
+            f = request.files['file']
+            basepath = os.path.dirname(__file__)
+            # save uploaded image into the uploads folder
+            filepath = os.path.join(basepath, 'uploads', f.filename)
+            f.save(filepath)
+
+            # perform image prediction
+            global imgpath
+            predict_img.imgpath = f.filename
+            img = cv2.imread(filepath)
+            frame = cv2.imencode('.jpg', cv2.UMat(img))[1].toBytes()
+            image = Image.open(io.BytesIO(frame))
+            yolo = YOLO('yolo_filename.pt')
+            detections = yolo.predict(image, save=True)
+            return display(f.filename)
+
+@app.route('/<path:filename>')
+def display(filename):
+    folder_path = 'runs/detect'
+    files = os.listdir(folder_path)
+    latest_file = files[0]
+    environ = request.environ
+
+    return send_from_directory(folder_path, latest_file, environ)
 
 # """Interacting with the classifier module"""
 # # First check whether the target directory already exists - this will save previous results being overwritten accidently. Consider having a popup confirm intention with user

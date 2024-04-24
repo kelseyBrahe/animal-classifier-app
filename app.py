9e21cd8f5deaf04a549a4163801271f0fd2e3ead
Kelsey Brahe Grover <kgrover3@myune.edu.au>
2024-04-24 14:53:29 -0600

    Created a function that allows users to upload multiple files and saves them to the '/uploads' folder. Also started the predict function by capturing whether the bounding box option is selected.

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from flask import render_template
 from PIL import Image
-from werkzeug.utils import send_from_directory
-import cv2
-import numpy as np
+from werkzeug.utils import send_from_directory, secure_filename
+# import cv2
+# import tkinter as tk
+# from tkinter import filedialog
+# import numpy as np
 import os
 from ultralytics import YOLO
 from classifer import get_img_classification, check_whether_target_directory_exists
 
+UPLOAD_FOLDER = '/uploads'
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def hello_world():
@@ -16,42 +21,42 @@ def hello_world():
         "index.html"
     )
 
-@app.route('/', methods=["GET", "POST"])
-def getVals():
+#
+# This function allows the user to upload multiple files that
+# will save to the /uploads folder.
+# From here we can loop through the images to process them.
+# We will need to clean the uploads folder after processing the images.
+@app.route('/upload', methods=["GET", "POST"])
+def uploadFiles():
     if request.method == "POST":
-        boundingBox = request.form.get("boundingBox")
+        basepath = os.path.dirname(__file__)
+        files = request.files.getlist('files')
+        if 'files' in request.files:
+            try:
+                for f in files:
+                    filename = secure_filename(f.filename)
+                    filepath = os.path.join(basepath, 'uploads', filename)
+                    f.save(filepath)
+                return render_template("index.html")
+            except FileNotFoundError:
+                print('File not found.')
+                return render_template("index.html")
+
+#
+# This function will get the value of the bounding box and call
+# the methods for image prediction.
+# If the bounding box is selected 'boundingBox' will equal "on", else None
+# It will need to process all of the images in the uploads folder,
+# save them to the /results folder, call a display function and then
+# clean the uploads folder.
+@app.route('/predict', methods=["GET", "POST"])     
+def predict():
+    if request.method == "POST":
+        # this is the value of the bounding box
+        boundingBox = request.form.get("bBox")
+        print(boundingBox == "on")
     return render_template("index.html")
 
-# this allows the upload of a single file
-# nothing is done with it currently and it is not saved anywhere
-# @app.route('/', methods=['GET', 'POST'])
-# def predict_img():
-#     if request.method == "POST":
-#         if 'file' in request.files:
-#             f = request.files['file']
-#             basepath = os.path.dirname(__file__)
-#             # save uploaded image into the uploads folder
-#             filepath = os.path.join(basepath, 'uploads', f.filename)
-#             f.save(filepath)
-
-#             # perform image prediction
-#             global imgpath
-#             predict_img.imgpath = f.filename
-#             img = cv2.imread(filepath)
-#             frame = cv2.imencode('.jpg', cv2.UMat(img))[1].toBytes()
-#             image = Image.open(io.BytesIO(frame))
-#             yolo = YOLO('yolo_filename.pt')
-#             detections = yolo.predict(image, save=True)
-#             return display(f.filename)
-
-# @app.route('/<path:filename>')
-# def display(filename):
-#     folder_path = 'runs/detect'
-#     files = os.listdir(folder_path)
-#     latest_file = files[0]
-#     environ = request.environ
-
-#     return send_from_directory(folder_path, latest_file, environ)
 
 # """Interacting with the classifier module"""
 # # First check whether the target directory already exists - this will save previous results being overwritten accidently. Consider having a popup confirm intention with user
